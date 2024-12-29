49c40b63a0abb6ea277c5a907f98264cb2e41c88
自由的世界人 <3196812536@qq.com>
2024-12-30 00:51:26 +0800

    fix: change source

--- web/src/utils/env.ts
+++ web/src/utils/env.ts
@@ -3,6 +3,6 @@
 // export const OAUTH2_REDIRECT_URI = 'http://localhost:5173/oauth2/redirect'
 // export const GITHUB_AUTH_URL = API_BASE_URL + '/oauth2/authorize/github?redirect_uri=' + OAUTH2_REDIRECT_URI;
 // production dev
-export const API_BASE_URL = 'http://localhost:8081';
+export const API_BASE_URL = 'https://backend.gitstar.center';
 export const OAUTH2_REDIRECT_URI = 'https://gitstar.center/oauth2/redirect'
 export const GITHUB_AUTH_URL = API_BASE_URL + '/oauth2/authorize/github?redirect_uri=' + OAUTH2_REDIRECT_URI;
